c830d6d702e9bacfad0990ffb14a17477ecb064b
dvansteen <dvansteen@tamu.edu>
2020-09-20 15:05:25 -0500

    events management skeleton

---
+++ app/controllers/events_controller.rb
@@ -0,0 +1,50 @@
+class EventsController < ApplicationController
+  def index
+    @events = Event.order(:title)
+  end
+
+  def show
+    @event = Event.find(params[:id])
+  end
+
+  def new
+    @event = Event.new
+  end
+
+  def create
+    @event = Event.new(event_params)
+    if @event.save
+      redirect_to(events_path)
+    else
+      render('new')
+    end
+  end
+
+  def edit
+    @event = Event.find(params[:id])
+  end
+
+  def update
+    @event = Event.find(params[:id])
+    if @event.update_attributes(event_params)
+      redirect_to(events_path(@event))
+    else
+      render('edit')
+    end
+  end
+
+  def delete
+    @event = Event.find(params[:id])
+  end
+
+  def destroy
+    @event = Event.find(params[:id])
+    @event.destroy
+    redirect_to(events_path)
+  end
+
+  private
+  def event_params
+    params.require(:event).permit(:name, :date, :time, :type, :attendance_points)
+  end
+end
