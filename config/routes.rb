f1638b5c9753a1684bdcc96d319501fefa5a027b
johnwmarrs-coding <johnwmarrs.coding@gmail.com>
2020-09-22 16:56:50 -0600

    Added basic authentication system

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
+
+  get 'dashboard/show'
+  root 'home#show'
+  
+  get 'home/show'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  get 'auth/auth0/callback' => 'auth0#callback'
+
+  get 'auth/failure' => 'auth0#failure'
+
+  get '/logout' => 'logout#logout'
+
+  get 'dashboard' => 'dashboard#show'
 end
