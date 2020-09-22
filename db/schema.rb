516266048639be8a28945a33dc754978cd19976b
pulchroxloom <dantevbarbieri@gmail.com>
2020-09-22 10:24:40 -0500

    Add schema change, forgot to stage

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_21_183256) do
+ActiveRecord::Schema.define(version: 2020_09_22_151737) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -66,7 +66,7 @@ ActiveRecord::Schema.define(version: 2020_09_21_183256) do
 
   create_table "members", force: :cascade do |t|
     t.text "name"
-    t.text "email"
+    t.text "email", null: false
     t.integer "class_year"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
