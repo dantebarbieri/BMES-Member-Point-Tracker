69175d70731a518ea165851c7e9bc2910f596df1
Sunil Babu <sunilp@tamu.edu>
2020-09-24 16:44:12 -0500

    Add data validation

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,14 @@
 class Event < ApplicationRecord
 	has_and_belongs_to_many :members
+	validates presence of :name
+	validates presence of :date
+	validates presence of :time
+	validates presence of :event_type
+	validates presence of :attendance_points
+	validate :event_date_cannot_be_in_the_past
+  def event_date_cannot_be_in_the_past
+    if :date.present? && :date < Date.today
+      errors.add(:date, "can't be in the past")
+    end
+  end
 end
