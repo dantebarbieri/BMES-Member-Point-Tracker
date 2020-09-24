de1e50f04f1bfe1d3c3e51162cf752a970621b3f
Shivani Iyer <shivani.iyer@yahoo.com>
2020-09-24 15:04:15 -0500

    crud changes

--- app/controllers/accomplishments_controller.rb
+++ app/controllers/accomplishments_controller.rb
@@ -1,28 +1,54 @@
 class AccomplishmentsController < ApplicationController
-   def index
-    @accomplishments = Accomplishments.all
+  def index
+    @accomplishments = Accomplishment.all
   end
 
   def show
-    @accomplishments = Accomplishments.find(params[:id])
+    @accomplishments = Accomplishment.find(params[:id])
   end
 
-  def edit
-    @accomplishments = Accomplishments.find(params[:id])
+  def new
+    @accomplishments = Accomplishment.new
   end
 
-  def update
-    @accomplishments = Accomplishments.find(params[:id])
-    if @accomplishments.update_attributes(accomplishments)
-      flash[:notice] = "Accomplishment #{@accomplishment.name} Updated Successfully"
-      redirect_to(accomplishments_path(@accomplishments))
+  def create
+    @accomplishments = Accomplishment.new(accomplishments_params)
+    if @accomplishments.save
+      flash[:notice] = "Accomplishment #{@accomplishment.name} Created Successfully"
+      redirect_to(accomplishments_path)
     else
-      render('edit')
+      render('new')
     end
   end
 
+  def edit
+    @accomplishments = Accomplishment.find(params[:id])
+  end
+
+  def update
+    @accomplishments = Accomplishment.find(params[:id])
+    session[:return_to] ||= request.referer
+    if @accomplishments.update_attributes(accomplishments_params)
+      flash[:notice] = "Accomplishment #{@accomplishment.name} Updated Successfully"
+      redirect_to(accomplishments_path(@accomplishments))
+    else
+      redirect_to session.delete(:return_to)
+    end
+  end
+
+  def delete
+    @accomplishments = Accomplishment.find(params[:id])
+  end
+
+  def destroy
+    @accomplishments = Accomplishment.find(params[:id])
+    @accomplishments.destroy
+    flash[:notice] = "Accomplishment #{@accomplishment.name} Destroyed Successfully"
+    redirect_to(accomplishments_path)
+  end
+
   private
-  def accomplishments
+  def accomplishments_params
     params.require(:accomplishments).permit(:name, :description, :points)
   end
 end
