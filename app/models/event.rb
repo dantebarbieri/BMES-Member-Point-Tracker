7e14e8896919881799450aa8fb602d8b0ae4bd1a
dvansteen <dvansteen@tamu.edu>
2020-09-24 20:11:26 -0500

    event: remove future date

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,14 +1,8 @@
 class Event < ApplicationRecord
-	has_and_belongs_to_many :members
-	validates presence of :name
-	validates presence of :date
-	validates presence of :time
-	validates presence of :event_type
-	validates presence of :attendance_points
-	validate :event_date_cannot_be_in_the_past
-  def event_date_cannot_be_in_the_past
-    if :date.present? && :date < Date.today
-      errors.add(:date, "can't be in the past")
-    end
-  end
+  has_and_belongs_to_many :members
+  validates :name, presence: true
+  validates :date, presence: true
+  validates :time, presence: true
+  validates :event_type, presence: true
+  validates :attendance_points, numericality: true, presence: true
 end
