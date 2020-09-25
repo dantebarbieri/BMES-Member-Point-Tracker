45d3ea36973198d0a967f899e3fcab2e18b1971d
dvansteen <dvansteen@tamu.edu>
2020-09-24 20:04:47 -0500

    events: style update

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,8 @@
 class Event < ApplicationRecord
-	has_and_belongs_to_many :members
+  has_and_belongs_to_many :members
+  validates :name, presence: true
+  validates :date, presence: true
+  validates :time, presence: true
+  validates :event_type, presence: true
+  validates :attendance_points, numericality: true, presence: true
 end
