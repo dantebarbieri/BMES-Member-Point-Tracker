64ce124df3eb4c2e30a3f5854aa18292bc744dc3
johnwmarrs-coding <johnwmarrs.coding@gmail.com>
2020-10-06 12:21:18 -0600

    Added feature to prevent multiple email signup for same user

--- app/controllers/concerns/existing_user.rb
+++ app/controllers/concerns/existing_user.rb
@@ -13,8 +13,12 @@ module ExistingUser
   end
 
   def existing_user?
-    @supplied_email = session[:app_user]['email']
-    @member = Member.find_by_email(@supplied_email)
+    #@supplied_email = session[:app_user]['email']
+    #@member = Member.find_by_email(@supplied_email)
+
+    #Need to find user by id
+    @supplied_uid = session[:app_user]['uid']
+    @member = Member.find_by_uid(@supplied_uid)
 
     redirect_to signup_path, alert: 'You still need to sign up!' unless @member
   end
--- app/controllers/profile_controller.rb
+++ app/controllers/profile_controller.rb
@@ -5,18 +5,30 @@ class ProfileController < ApplicationController
   include ExistingUser
 
   def show
-    @supplied_email = session[:app_user]['email']
-    @member = Member.find_by_email(@supplied_email)
+    #@supplied_email = session[:app_user]['email']
+    #@member = Member.find_by_email(@supplied_email)
+
+    #Need to find user by id
+    @supplied_uid = session[:app_user]['uid']
+    @member = Member.find_by_uid(@supplied_uid)
   end
 
   def edit
-    @supplied_email = session[:app_user]['email']
-    @member = Member.find_by_email(@supplied_email)
+    #@supplied_email = session[:app_user]['email']
+    #@member = Member.find_by_email(@supplied_email)
+
+    #Need to find user by id
+    @supplied_uid = session[:app_user]['uid']
+    @member = Member.find_by_uid(@supplied_uid)
   end
 
   def update
-    @supplied_email = session[:app_user]['email']
-    @member = Member.find_by_email(@supplied_email)
+    #@supplied_email = session[:app_user]['email']
+    #@member = Member.find_by_email(@supplied_email)
+
+    #Need to find user by id
+    @supplied_uid = session[:app_user]['uid']
+    @member = Member.find_by_uid(@supplied_uid)
 
     if @member.update(member_params)
       redirect_to profile_path, notice: 'Your data was updated.'
