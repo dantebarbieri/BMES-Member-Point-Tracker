0a2d0bbdf4d9cc9a308d5416db76403269049fb4
pulchroxloom <dantevbarbieri@gmail.com>
2020-10-06 18:17:26 -0500

    Add beginnings of point controller

---
+++ test/controllers/points_controller_test.rb
@@ -0,0 +1,19 @@
+require 'test_helper'
+
+class PointsControllerTest < ActionDispatch::IntegrationTest
+  test "should get events" do
+    get points_events_url
+    assert_response :success
+  end
+
+  test "should get manual_points" do
+    get points_manual_points_url
+    assert_response :success
+  end
+
+  test "should get accomplishments" do
+    get points_accomplishments_url
+    assert_response :success
+  end
+
+end
